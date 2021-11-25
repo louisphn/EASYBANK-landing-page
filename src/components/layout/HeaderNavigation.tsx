73b6e4d20d69c24842e2da8df64cac73fc89d656
Louis Phan <72560298+louisphn@users.noreply.github.com>
2021-11-25 20:45:11 +0900

    split data to separate file

--- src/components/layout/HeaderNavigation.tsx
+++ src/components/layout/HeaderNavigation.tsx
@@ -1,18 +1,19 @@
 import { VFC, memo} from 'react'
 import { useRouter } from 'next/router'
 
+import { headerMenu } from '../../lib/data';
+
 interface Props {
   active: boolean;
 }
 
 const HeaderNavigation: VFC<Props> = ({active}) => {
   const router = useRouter();
-  const menu = ['Home', 'About', 'Contact', 'Blog', 'Careers'];
 
   if (!active) return <></>
   return <nav className="absolute z-10 top-[72px] left-0 w-screen h-screen bg-gradient-to-b from-primary-dark-blue to-transparent lg:w-screen lg:min-w-auto lg:h-[64px] lg:top-0 lg:bg-none">
     <ul className="absolute transform left-[50%] top-8 translate-x-[-50%] w-11/12 bg-white flex flex-col items-center rounded-xl py-8 lg:top-0 lg:transform lg:justify-between lg:w-4/12 lg:mt-[2px] lg:min-w-[468px] lg:bg-transparent lg:py-0 lg:flex-row">
-    {menu.map((item) => (
+    {headerMenu.map((item) => (
       <li key={item.toLowerCase()} onClick={() => router.push(`/${item.toLowerCase()}`)}
       className="text-primary-dark-blue my-4 text-xl"
       >
---
+++ src/lib/data.ts
@@ -0,0 +1,30 @@
+export const footerMenu = [
+  {
+    name: 'About Us',
+    path: '/about'
+  },
+  {
+    name: 'Contact',
+    path: '/contact'
+  },
+  {
+    name: 'Blog',
+    path: '/blog'
+  },
+  {
+    name: 'Careers',
+    path: '/careers'
+  },
+  {
+    name: 'Support',
+    path: '/support'
+  },
+  {
+    name: 'Privacy Policy',
+    path: '/privacy-policies'
+  },
+];
+
+export const headerMenu = [
+  'Home', 'About', 'Contact', 'Blog', 'Careers'
+]
