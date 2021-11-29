091bf38a5d0c78ec454ed4c52307c652bb2223ff
Louis Phan <louisphan@notreallouisss.local>
2021-11-29 14:55:16 +0900

    fix background position

--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -6,8 +6,8 @@ import PrimaryButton from '../button/PrimaryButton';
 
 const Heading: VFC = () => {
   return (
-    <section className="w-screen min-h-full flex items-center pb-16 md:pb-0">
-      <div className="bg-white min-w-[320px] w-screen flex flex-col justify-center items-start md:flex-row md:justify-between md:items-center md:overflow-hidden md:min-w-auto">
+    <section className="w-screen min-h-full max-h-[800px] flex items-center pb-16 md:pb-0">
+      <div className="bg-white min-w-[320px] w-screen flex flex-col justify-center items-start md:flex-row md:justify-between md:items-center md:min-w-auto">
         <div className="relative w-full md:w-8/12 md:order-2">
           <div className="block w-full min-w-[320px] h-[284px] overflow-hidden md:hidden">
             <Image
@@ -30,7 +30,7 @@ const Heading: VFC = () => {
               alt={'background-image'}
             />
           </div>
-          <div className="absolute block w-[90vw] bottom-0 left-[50%] transform translate-x-[-50%] md:translate-x-[-30%] md:w-11/12 md:-bottom-16">
+          <div className="absolute block w-[90vw] bottom-0 left-[50%] transform translate-x-[-50%] md:translate-x-[-30%] md:w-11/12 md:-bottom-16 xl:bottom-[-128px]">
             <Image
               priority
               src={'/assets/images/image-mockups.png'}
